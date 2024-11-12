385dfc2e8435db8e02717180423a62cb47ff1304
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-11-12 16:16:23 -0500

    Updated all the views

--- src/main/java/view/ChooseOptionsView.java
+++ src/main/java/view/ChooseOptionsView.java
@@ -1,4 +1,17 @@
 package view;
 
-public class ChooseOptionsView {
+import javax.swing.*;
+
+public class ChooseOptionsView extends GeneralView {
+
+    public ChooseOptionsView() {
+
+    }
+
+    public static void main(String[] args) {
+        ChooseOptionsView chooseOptionsView = new ChooseOptionsView();
+        chooseOptionsView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        chooseOptionsView.setVisible(true);
+        chooseOptionsView.setResizable(false);
+    }
 }
--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,4 +1,32 @@
 package view;
 
-public class ViewManager {
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import interface_adapter.ViewManagerModel;
+
+/**
+ * The View Manager for the program. It listens for property change events
+ * in the ViewManagerModel and updates which View should be visible.
+ */
+public class ViewManager implements PropertyChangeListener {
+    private final CardLayout cardLayout;
+    private final JPanel views;
+    private final ViewManagerModel viewManagerModel;
+
+    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
+        this.views = views;
+        this.cardLayout = cardLayout;
+        this.viewManagerModel = viewManagerModel;
+        this.viewManagerModel.addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("state")) {
+            final String viewModelName = (String) evt.getNewValue();
+            cardLayout.show(views, viewModelName);
+        }
+    }
 }

dd574b967a6e74e9c171605828941272b22085fc
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-11-15 19:12:35 -0500

    Added more to the views

--- src/main/java/view/ChooseOptionsView.java
+++ src/main/java/view/ChooseOptionsView.java
@@ -4,8 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChooseOptionsView extends GeneralView {
+    JPanel panel;
 
     public ChooseOptionsView() {
+
+        // Add in the instruction text at the top
+        JLabel instructionsLabel = new JLabel("Please choose your desired preferences below, and we will plan" +
+                " your day for you.", JLabel.CENTER);
+        instructionsLabel.setFont(getMontserratBoldItalicFont());
+        instructionsLabel.setForeground(new Color(47, 62, 70));
+        instructionsLabel.setBounds(0, 40, getBackgroundImage().getIconWidth(), 30);
+        getBackgroundLabel().add(instructionsLabel);
+
+        // Everything below this comment will be the input a user can give
+        JLabel radiusLabel = new JLabel("Radius (km):", JLabel.LEFT);
+        radiusLabel.setFont(getMontserratFont());
+        radiusLabel.setForeground(new Color(47, 62, 70));
+        radiusLabel.setBounds(95, 100, getBackgroundImage().getIconWidth(), 30);
+        getBackgroundLabel().add(radiusLabel);
+
+        JSlider radiusSlider = new JSlider(0, 50, 25);
+        radiusSlider.setMajorTickSpacing(10);
+        radiusSlider.setMinorTickSpacing(1);
+        radiusSlider.setPaintTicks(true);
+        radiusSlider.setPaintLabels(true);
+        radiusLabel.setBounds(150, 100, 300, 40);
+        getBackgroundLabel().add(radiusSlider);
+
         // Add in the continue button
         JButton startButton = new JButton("Continue");
         startButton.setFont(getMontserratFont());

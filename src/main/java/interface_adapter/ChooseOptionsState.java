a29dbe2f97f15eb15e6deb29c04f61f52e0b043a
Yiping Chen <yping.chen@mail.utoronto.ca>
2024-11-23 18:45:12 -0500

    Create ChooseOptionsState

--- src/main/java/interface_adapter/ChooseOptionsState.java
+++ src/main/java/interface_adapter/ChooseOptionsState.java
@@ -1,5 +1,46 @@
 package interface_adapter;
 
+import entity.CommonLocationData;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ChooseOptionsState {
-    private Location startingLocation = new Location();
+    private CommonLocationData startingLocation = new CommonLocationData();
+    private double maxDistance = 0.0;
+    private double minStars = 0.0;
+    private SimpleDateFormat startTime = new SimpleDateFormat("HH:mm");
+    private SimpleDateFormat endTime = new SimpleDateFormat("HH:mm");
+    private List<String> possibleLocationTypes = new ArrayList<String>();
+
+    public CommonLocationData getStartingLocation() {
+        return startingLocation;
+    }
+    public double getMaxDistance() {
+        return maxDistance;
+    }
+    public double getMinStars() {
+        return minStars;
+    }
+    public List<String> getPossibleLocationTypes() {
+        return possibleLocationTypes;
+    }
+
+    public void setStartingLocation(CommonLocationData startingLocation) {
+        this.startingLocation = startingLocation;
+    }
+    public void setMaxDistance(double maxDistance) {
+        this.maxDistance = maxDistance;
+    }
+    public void setMinStars(double minStars) {
+        this.minStars = minStars;
+    }
+    public void setPossibleLocationTypes(List<String> possibleLocationTypes) {
+        this.possibleLocationTypes = possibleLocationTypes;
+    }
+
+
+
 }

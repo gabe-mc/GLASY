dc6e170dbded630cbdea0290a3e6e25e38e1d5af
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-11-23 20:25:02 -0500

    Updated the views so they inherit the JFrame class

--- src/main/java/view/DisplayOptionsView.java
+++ src/main/java/view/DisplayOptionsView.java
@@ -5,10 +5,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DisplayOptionsView extends LoadFonts {
+public class DisplayOptionsView extends JFrame {
     private ImageIcon bkgImage;
     private JLabel backgroundLabel;
-    private JFrame mainFrame;
 
     public DisplayOptionsView() {
         LoadFonts loadFonts = new LoadFonts();
@@ -16,19 +15,16 @@ public class DisplayOptionsView extends LoadFonts {
         // Initialize the image
         this.bkgImage = new ImageIcon("src/main/java/view/images/BackgroundImage2.png");
 
-        // Make the frame
-        this.mainFrame = new JFrame();
-
         // Set the background image
         this.backgroundLabel = new JLabel(this.bkgImage);
 
-        this.mainFrame.add(this.backgroundLabel, BorderLayout.CENTER);
-        this.mainFrame.setSize(this.bkgImage.getIconWidth() + 10, this.bkgImage.getIconHeight() + 30);
+        add(this.backgroundLabel, BorderLayout.CENTER);
+        setSize(this.bkgImage.getIconWidth() + 10, this.bkgImage.getIconHeight() + 30);
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (screenSize.width - bkgImage.getIconWidth()) / 2;
         int y = (screenSize.height - bkgImage.getIconHeight()) / 2;
-        this.mainFrame.setLocation(x, y);
+        setLocation(x, y);
 
         // Add in the continue button
         JButton startButton = new JButton("Continue");
@@ -37,9 +33,9 @@ public class DisplayOptionsView extends LoadFonts {
         startButton.setBackground(new Color(202, 210, 197));
         startButton.setBounds((this.bkgImage.getIconWidth() - 200) / 2 - 50, 600, 300, 40);
         this.backgroundLabel.add(startButton);
-        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.mainFrame.setVisible(true);
-        this.mainFrame.setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
     }
 
     public static void main(String[] args) {

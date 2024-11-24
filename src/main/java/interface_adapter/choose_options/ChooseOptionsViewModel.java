eb4c4f961c3f819c356a6290d33038b3c1727edc
Yiping Chen <yping.chen@mail.utoronto.ca>
2024-11-23 19:55:55 -0500

    View Model: choose options + view model

--- src/main/java/interface_adapter/choose_options/ChooseOptionsViewModel.java
+++ src/main/java/interface_adapter/choose_options/ChooseOptionsViewModel.java
@@ -1,4 +1,6 @@
-package interface_adapter;
+package interface_adapter.choose_options;
+
+import interface_adapter.ViewModel;
 
 public class ChooseOptionsViewModel extends ViewModel<ChooseOptionsState> {
 

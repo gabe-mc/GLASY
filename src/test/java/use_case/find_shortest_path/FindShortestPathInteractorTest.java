2838d2dd737f0c7cdcf166abf5e6d58a719ec5fb
Gabriel McFadyen <gabrielmcfadyen@gmail.com>
2024-11-26 23:02:47 -0500

    Changed FindShortestPath Use case to use AttractionData and fill out TravelTime

--- src/test/java/use_case/find_shortest_path/FindShortestPathInteractorTest.java
+++ src/test/java/use_case/find_shortest_path/FindShortestPathInteractorTest.java
@@ -1,6 +1,8 @@
 package use_case.find_shortest_path;
 
 import data_access.GoogleMapsLocationProvider;
+import entity.AttractionData;
+import entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,19 +13,36 @@ public class FindShortestPathInteractorTest {
 
     @Test
     void SuccessTest() throws IOException {
-        ArrayList<String> addresses = new ArrayList<>();
-        addresses.add("15 Church St, Toronto ON");
-        addresses.add("197 Yonge St, Toronto ON");
-        addresses.add("101 Yonge St, Toronto ON");
+        AttractionData attraction1 = new AttractionData();
+        attraction1.setAddress("15 Church St, Toronto ON");
+        AttractionData attraction2 = new AttractionData();
+        attraction2.setAddress("197 Yonge St, Toronto ON");
+        AttractionData attraction3 = new AttractionData();
+        attraction3.setAddress("101 Yonge St, Toronto ON");
+        ArrayList<AttractionData> addresses = new ArrayList<>();
+        addresses.add(attraction1);
+        addresses.add(attraction2);
+        addresses.add(attraction3);
         FindShortestPathOutputBoundary outputBoundary = new FindShortestPathOutputBoundary() {
 
             @Override
             public void presentOutput(FindShortestPathOutputData outputData) {
-                ArrayList<String> expected = new ArrayList<>();
-                expected.add("15 Church St, Toronto ON");
-                expected.add("101 Yonge St, Toronto ON");
-                expected.add("197 Yonge St, Toronto ON");
-                Assertions.assertEquals(expected, outputData.getShortestPath());
+                AttractionData attraction11 = new AttractionData();
+                attraction11.setAddress("15 Church St, Toronto ON");
+                AttractionData attraction22 = new AttractionData();
+                attraction22.setAddress("197 Yonge St, Toronto ON");
+                AttractionData attraction33 = new AttractionData();
+                attraction33.setAddress("101 Yonge St, Toronto ON");
+                ArrayList<String> addresses11 = new ArrayList<>();
+                addresses11.add(attraction11.getAddress());
+                addresses11.add(attraction33.getAddress());
+                addresses11.add(attraction22.getAddress());
+                ArrayList<AttractionData> output = outputData.getShortestPath();
+                ArrayList<String> outputStr = new ArrayList<>();
+                for (AttractionData put : output) {
+                    outputStr.add(put.getAddress());
+                }
+                Assertions.assertEquals(addresses11, outputStr);
             }
         };
         FindShortestPathInputData inputData = new FindShortestPathInputData(addresses);

3831b156312f4c234f326f7fc0ae34d748659999
Steven Lin <yuchenglin0705@gmail.com>
2024-11-27 19:23:12 -0500

    Changes: ComputeTime use case changed to adhere to Setting class' startTime and endTime variables being Date instead of double.

---
+++ src/main/java/use_case/compute_time/ComputeTimeUserDataAccessInterface.java
@@ -0,0 +1,10 @@
+package use_case.compute_time;
+
+import entity.Settings;
+
+/**
+ * DAO for the Compute Time Use Case.
+ */
+public interface ComputeTimeUserDataAccessInterface {
+    Settings getSettings();
+}

c1e116c8772fedd9f9c87940882e3301ddfb13a1
Lily Phan <79292581+pH-li@users.noreply.github.com>
2024-12-01 14:38:19 -0500

    Changed the files to be saved as .md, and also removed some comments

--- src/main/java/use_case/save_itinerary/SaveItineraryInteractor.java
+++ src/main/java/use_case/save_itinerary/SaveItineraryInteractor.java
@@ -2,6 +2,8 @@ package use_case.save_itinerary;
 
 import java.io.FileWriter;
 import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * The Save Itinerary Interactor.
@@ -15,12 +17,18 @@ public class SaveItineraryInteractor implements SaveItineraryInputBoundary {
 
     @Override
     public void execute(SaveItineraryInputData inputData) {
-        String filePath = inputData.getFilePath() + "/" + inputData.getFileName() + ".txt";
+        String filePath = inputData.getFilePath() + "/" + inputData.getFileName() + ".md";
+        SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
+        String date = ft.format(new Date());
         try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write("Itinerary Planner\n");
+            writer.write("# Your Itinerary\n");
+            writer.write("## " + date + "\n\n");
             List<String[]> itinerary = inputData.getContent();
             for (String[] info : itinerary) {
-                writer.write(info[0] + " - " + info[1] + "(" + info[2] + ")\n");
+                writer.write("### " + info[0] + "\n");
+                writer.write(info[1] + "\n");
+                writer.write("");
+                writer.write("*" + info[2] + "*\n\n");
             }
         } catch (Exception e) {
             e.printStackTrace();

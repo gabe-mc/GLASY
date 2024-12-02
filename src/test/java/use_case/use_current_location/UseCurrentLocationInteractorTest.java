3f295e8fe02b1f7e07b02f40a6a0eeb2adf0a3dc
Yiping Chen <yping.chen@mail.utoronto.ca>
2024-12-01 21:48:02 -0500

    Finished UseCurrentLocationInteractorTest

--- src/test/java/use_case/use_current_location/UseCurrentLocationInteractorTest.java
+++ src/test/java/use_case/use_current_location/UseCurrentLocationInteractorTest.java
@@ -1,36 +1,34 @@
 package use_case.use_current_location;
 
-import entity.CommonLocationData;
-import entity.LocationData;
+import data_access.CurrentLocationProvider;
+import data_access.UserDataAccessObject;
+import entity.AttractionData;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.stream.Location;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
-public class UseCurrentLocationInteractorTest {
-
+class UseCurrentLocationTest {
     @Test
-    public void successTest(){
-        final UseCurrentLocationUserDataAccessInterface userDataAccessObject = new UseCurrentLocationUserDataAccessInterface() {
-            @Override
-            public LocationData getCurrentLocation() {
-                return null;
-            }
-        };
-        final UseCurrentLocationOutputBoundary chooseOptionsPresenter;
-        LocationData currentLocation = userDataAccessObject.getCurrentLocation();
-        UseCurrentLocationOutputBoundary outputBoundary = new UseCurrentLocationOutputBoundary() {
+    void successTest() {
+        CurrentLocationProvider currentLocationProvider = new CurrentLocationProvider();
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
+        AttractionData currentLocation = currentLocationProvider.getLocation();
+        userDataAccessObject.setCurrentLocation(currentLocation);
+
+        UseCurrentLocationOutputBoundary successPresenter = new UseCurrentLocationOutputBoundary() {
             @Override
             public void updateCurrentLocation(UseCurrentLocationOutputData outputData) {
-                assertEquals(currentLocation, outputData.getCurrentLocation());
+                assertEquals(currentLocation.getAddress(), outputData.getCurrentLocation());
             }
 
             @Override
             public void prepareFailView(String errorMessage) {
-                fail("UseCurrentLocation failed");
+                fail("Use case failure is unexpected.");
             }
         };
+
+        UseCurrentLocationInputBoundary interactor = new UseCurrentLocationInteractor(userDataAccessObject,
+                successPresenter);
+        interactor.useCurrentLocation();
     }
 }
